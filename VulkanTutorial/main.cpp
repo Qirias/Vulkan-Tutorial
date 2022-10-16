bb65373ce6553fd020dd525bec4ecbf58c2133c6
Qirias <kiriakostr8@gmail.com>
2022-10-16 22:36:43 +0300

    Initial Commit

---
+++ VulkanTutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VulkanTutorial
+//
+//  Created by Kiriakos Gavras on 16/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
